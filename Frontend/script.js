b885145c25e2f1b7d76837cb36a860178c6a2a26
Bhoomika <rudrakshchaudhary451@gmail.com>
2025-11-10 12:49:55 +0530

    Initialize tsParticles with custom settings
    Added particle effect configuration using tsParticles.

--- Frontend/script.js
+++ Frontend/script.js
@@ -9,3 +9,48 @@ buttons.forEach(btn => {
 });
 
 
+tsParticles.load("particles-js", 
+  {
+    background: 
+    {
+      color: {value: "transparent"}
+    },
+    fpsLimit: 60,
+    particles: 
+    {
+      number: 
+      {
+        value: 77,
+        density: 
+        {
+          enable: true,
+          area: 800
+        }
+      },
+      color: {value: "#7d7dfbff"},
+      shape: {type: "star"},
+      opacity: 
+      {
+        value: 0.8,
+        random : true
+      },
+      size: 
+      {
+        value: 2,
+        random: true
+      },
+      move: 
+      {
+        enable: true,
+        speed: 0.6,
+        direction: "none",
+        outModes: "out"
+
+      }
+    },
+    detectRetina: true
+  }
+)
+
+
+
